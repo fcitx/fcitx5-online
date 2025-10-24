cf04927fd8c7383737149cc70da868ef78820366
Qijia Liu <liumeo@pku.edu.cn>
2025-10-23 21:16:23 -0400

    unify outline icons; add reset button (#8)

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: 'tests',
+  retries: 3,
+  expect: {
+    timeout: 10000,
+  },
   fullyParallel: true,
   webServer: {
     command: 'pnpm run preview',

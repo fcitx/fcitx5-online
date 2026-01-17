2a4713e4ef02f9717ce3981580bd8d1a8df8ed5c
Qijia Liu <liumeo@pku.edu.cn>
2026-01-16 20:06:17 -0500

    notification actions

--- src/components/MyContent.vue
+++ src/components/MyContent.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { ResetButton } from 'fcitx5-config-vue'
 import { fcitxReady } from 'fcitx5-js'
-import { NInput, NSpace, useMessage, useNotification } from 'naive-ui'
+import { NButton, NFlex, NInput, NSpace, useMessage, useNotification } from 'naive-ui'
+import { h } from 'vue'
 import { t } from '../i18n'
 import StatusArea from './StatusArea.vue'
 
@@ -10,18 +11,27 @@ const message = useMessage()
 let messagedSystemInputMethodInUse = false
 
 fcitxReady.then(() => {
-  window.fcitx.setNotificationCallback((name, icon, body, timeout) => {
-    const options = { title: name, content: body, duration: timeout }
-    switch (icon) {
-      case 'error':
-        notification.error(options)
-        break
-      case 'success':
-        notification.success(options)
-        break
-      default:
-        notification.info(options)
-    }
+  window.fcitx.setNotificationCallback((name, icon, body, timeout, tipId, actions) => {
+    const instance = notification.create({
+      type: icon === 'error' ? 'error' : icon === 'success' ? 'success' : 'info',
+      title: name,
+      content: body,
+      duration: timeout > 0 ? Math.max(timeout, 3000) : undefined,
+      meta: () => h('div'),
+      action: tipId
+        ? () => h(NButton, {
+            onClick: () => {
+              window.fcitx.activateNotificationAction('dont-show', tipId)
+              instance.destroy()
+            },
+          }, () => window.fcitx.translateDomain('fcitx5', 'Do not show again'))
+        : () => h(NFlex, () => actions.map(({ id, text }) => h(NButton, {
+            onClick: () => {
+              window.fcitx.activateNotificationAction(id)
+              instance.destroy()
+            },
+          }, () => text))),
+    })
   })
 
   window.fcitx.setSystemInputMethodInUseCallback(() => {

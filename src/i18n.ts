9cae040be110484aed1effa2a25186612a8618f1
Dmitry <87600810+gostsdmitry@users.noreply.github.com>
2026-08-06 00:21:25 +0700

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,14 @@
 import type { NLocale } from 'naive-ui'
 import { getLocale } from 'fcitx5-config-vue'
-import { enUS, zhCN } from 'naive-ui'
+import { enUS, zhCN, ruRU } from 'naive-ui'
 import { createI18n } from 'vue-i18n'
 import zh_CN from './locales/zh-CN.json'
+import zh_CN from './locales/ru-RU.json'
 
 const messages = {
   'en': Object.fromEntries(Object.keys(zh_CN).map(key => [key, key])),
   'zh-CN': zh_CN,
+  'ru-RU': ru_RU,
 }
 
 const i18n = createI18n({
@@ -23,5 +25,6 @@ export function getNaiveLocale(): NLocale {
   return {
     'en': enUS,
     'zh-CN': zhCN,
+    'ru-RU': ruRU,
   }[locale]
 }

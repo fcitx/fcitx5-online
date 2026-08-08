f7824b9a8bdbd34a65c22484d744e019b6edadef
Qijia Liu <liumeo@pku.edu.cn>
2026-08-08 19:28:08 -0400

    fixup

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,14 @@
 import type { NLocale } from 'naive-ui'
 import { getLocale } from 'fcitx5-config-vue'
-import { enUS, zhCN, ruRU } from 'naive-ui'
+import { enUS, ruRU, zhCN } from 'naive-ui'
 import { createI18n } from 'vue-i18n'
+import ru from './locales/ru.json'
 import zh_CN from './locales/zh-CN.json'
-import ru_RU from './locales/ru-RU.json'
 
 const messages = {
   'en': Object.fromEntries(Object.keys(zh_CN).map(key => [key, key])),
+  'ru': ru,
   'zh-CN': zh_CN,
-  'ru-RU': ru_RU,
 }
 
 const i18n = createI18n({
@@ -24,7 +24,7 @@ export function getNaiveLocale(): NLocale {
   const locale = i18n.global.locale
   return {
     'en': enUS,
+    'ru': ruRU,
     'zh-CN': zhCN,
-    'ru-RU': ruRU,
   }[locale]
 }

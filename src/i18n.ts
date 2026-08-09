74859c6c5b68759557b42a82a7ce62deff002a15
Dmitry <87600810+gostsdmitry@users.noreply.github.com>
2026-08-09 07:08:32 +0700

    Added Russian translation (#15)
    Co-authored-by: Qijia Liu <liumeo@pku.edu.cn>

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,13 @@
 import type { NLocale } from 'naive-ui'
 import { getLocale } from 'fcitx5-config-vue'
-import { enUS, zhCN } from 'naive-ui'
+import { enUS, ruRU, zhCN } from 'naive-ui'
 import { createI18n } from 'vue-i18n'
+import ru from './locales/ru.json'
 import zh_CN from './locales/zh-CN.json'
 
 const messages = {
   'en': Object.fromEntries(Object.keys(zh_CN).map(key => [key, key])),
+  'ru': ru,
   'zh-CN': zh_CN,
 }
 
@@ -22,6 +24,7 @@ export function getNaiveLocale(): NLocale {
   const locale = i18n.global.locale
   return {
     'en': enUS,
+    'ru': ruRU,
     'zh-CN': zhCN,
   }[locale]
 }
